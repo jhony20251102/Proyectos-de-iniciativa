d35ba1f815c640a20934f661054f5773d9131fb7
RICKY SEDANO REYES <rsedano@visivaedu.com>
2026-07-02 20:24:03 -0500

    Delete certus-asistente directory

--- certus-asistente/backend/src/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3001,
-    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || 'PON_TU_API_KEY_ELEVENLABS_AQUI'
-};
--- certus-asistente/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import NovaIA from "./components/CertusVirtualAssistant";
-
-function App() {
-  return <NovaIA />;
-}
-
-export default App;
